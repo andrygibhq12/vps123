17bccc3f002ca819c3bd65f551a49127514b4c18
andrygibhq12 <andrygibhq12@gmail.com>
2025-10-31 02:04:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sudo apt update
-
+wget http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_134.0.6998.165-1_amd64.deb
+sudo dpkg -i google-chrome-stable_134.0.6998.165-1_amd64.deb
+sudo apt --fix-broken install -y
 sudo apt update -y && sudo apt upgrade -y
 
 sudo apt install xfce4 xfce4-goodies xrdp -y
