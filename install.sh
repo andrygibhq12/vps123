8616907735d49dceb515f8a021dc2077f0c4bf31
andrygibhq12 <andrygibhq12@gmail.com>
2025-10-31 00:08:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,6 @@ sudo apt update
 
 sudo apt update -y && sudo apt upgrade -y
 
-wget http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_130.0.6723.58-1_amd64.deb
-sudo dpkg -i google-chrome-stable_130.0.6723.58-1_amd64.deb
-sudo apt --fix-broken install -y
-
 sudo apt install xfce4 xfce4-goodies xrdp -y
 
 echo "startxfce4" > ~/.xsession
