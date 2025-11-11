6d32a8f49efe989478f4b3c7e7f026cdf9256ec3
andrygibhq12 <andrygibhq12@gmail.com>
2025-11-11 11:15:35 +0700

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -7,9 +7,10 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb 
 sudo dpkg -i chrome-remote-desktop_current_amd64.deb > /dev/null 2>&1
 sudo apt --fix-broken install -y > /dev/null 2>&1
 
-echo "===> Installing Xfce4"
+echo "===> Installing Xfce"
 sudo apt install xfce4 xfce4-goodies -y > /dev/null 2>&1
 echo "exec xfce4-session" > ~/.chrome-remote-desktop-session
+chmod +x ~/.chrome-remote-desktop-session
 
 echo "===> Installing Gdown"
 sudo apt install python3-pip -y > /dev/null 2>&1
@@ -22,5 +23,3 @@ sudo apt --fix-broken install -y > /dev/null 2>&1
 
 echo "===> Installing Remmina"
 sudo apt install remmina remmina-plugin-rdp remmina-plugin-vnc remmina-plugin-secret > /dev/null 2>&1
-
-chmod +x ~/.chrome-remote-desktop-session
