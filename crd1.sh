5bb8433f9c9522199236da48c0baf5251563b424
andrygibhq12 <andrygibhq12@gmail.com>
2025-11-11 18:56:30 +0700

    Update crd1.sh

--- crd1.sh
+++ crd1.sh
@@ -5,15 +5,18 @@ sudo useradd -m gibhq
 sudo adduser gibhq sudo
 echo 'gibhq:gibhq' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
+
 sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 desktop-base
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-sudo apt install --assume-yes xscreensaver
-sudo systemctl disable lightdm.service
+sudo apt install --assume-yes xfce4 xfce4-goodies
+echo "exec xfce4-session" > ~/.chrome-remote-desktop-session
+chmod +x ~/.chrome-remote-desktop-session  
+sudo apt remove --assume-yes gnome-terminal
+
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
