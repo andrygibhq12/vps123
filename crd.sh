b31fbd2c1295f92feee09abab15fbab755c48789
andrygibhq12 <andrygibhq12@gmail.com>
2025-11-11 22:25:26 +0700

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -1,27 +1,64 @@
 #!/bin/bash
 
-sudo apt update -y
+{
+username="gibhq"
+password="gibhq"
+sudo useradd -m "$username"
+sudo adduser "$username" sudo
+echo "$username:$password" | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+} > /dev/null 2>&1
 
-echo "===> Installing CRD"
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb > /dev/null 2>&1
-sudo dpkg -i chrome-remote-desktop_current_amd64.deb > /dev/null 2>&1
-sudo apt --fix-broken install -y > /dev/null 2>&1
+CRP=""
+Pin=123456
 
+installCRD() {
+    echo "Installing Crd"
+    wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb > /dev/null 2>&1
+    sudo dpkg --install chrome-remote-desktop_current_amd64.deb > /dev/null 2>&1
+    sudo apt install --assume-yes --fix-broken > /dev/null 2>&1
+}
 
-echo "===> Installing Xfce"
-sudo apt install xfce4 xfce4-goodies -y > /dev/null 2>&1
-sudo apt remove gnome-terminal -y > /dev/null 2>&1
-sudo apt install remmina remmina-plugin-rdp remmina-plugin-vnc remmina-plugin-secret -y > /dev/null 2>&1
+installDesktopEnvironment() {
+    echo "Installing Xfce"
+    sudo apt install --assume-yes xfce4 xfce4-goodies > /dev/null 2>&1
+    echo "exec xfce4-session" > ~/.chrome-remote-desktop-session
+    chmod +x ~/.chrome-remote-desktop-session
+    sudo apt remove --assume-yes gnome-terminal > /dev/null 2>&1
+}
 
-echo "exec xfce4-session" > ~/.chrome-remote-desktop-session
-chmod +x ~/.chrome-remote-desktop-session
+installBrowser() {
+    echo "Installing Browser"
+    wget http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_130.0.6723.116-1_amd64.deb > /dev/null 2>&1
+    sudo dpkg --install google-chrome-stable_130.0.6723.116-1_amd64.deb > /dev/null 2>&1
+    sudo apt install --assume-yes --fix-broken > /dev/null 2>&1
+    sudo apt install --assume-yes remmina remmina-plugin-rdp remmina-plugin-vnc remmina-plugin-secret > /dev/null 2>&1
+    sudo apt install --assume-yes python3-pip > /dev/null 2>&1
+    sudo pip install gdown > /dev/null 2>&1
+}
 
+getCRP() {
+    echo "Check https://remotedesktop.google.com/headless"
+    read -p "SSH Code: " CRP
+    if [ -z "$CRP" ]; then
+        echo "Please enter a valid value."
+        getCRP
+    fi
+}
 
-echo "===> Installing Chrome"
-sudo apt install python3-pip -y > /dev/null 2>&1
-sudo pip install gdown > /dev/null 2>&1
-wget http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_130.0.6723.116-1_amd64.deb > /dev/null 2>&1
-sudo dpkg -i google-chrome-stable_130.0.6723.116-1_amd64.deb > /dev/null 2>&1
-sudo apt --fix-broken install -y > /dev/null 2>&1
+finish() {
+    sudo groupadd chrome-remote-desktop > /dev/null 2>&1
+    command="$CRP --pin=$Pin" > /dev/null 2>&1
+    sudo su - $username -c "$command" > /dev/null 2>&1
+    sudo systemctl status chrome-remote-desktop@$USER
+}
 
-echo "https://remotedesktop.google.com/headless"
+# Main
+sudo apt update
+installCRD
+installDesktopEnvironment
+installBrowser
+getCRP
+finish
+
+while true; do sleep 10; done
