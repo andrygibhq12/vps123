793aadcaf246f5fd8a2e4ffeb9cb244b1a318407
andrygibhq12 <andrygibhq12@gmail.com>
2025-11-11 13:38:06 +0700

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -8,7 +8,7 @@ sudo dpkg -i chrome-remote-desktop_current_amd64.deb > /dev/null 2>&1
 sudo apt --fix-broken install -y > /dev/null 2>&1
 
 echo "===> Installing Xfce"
-sudo apt install xfce4 desktop-base xfce4-terminal -y > /dev/null 2>&1
+sudo apt install xfce4 xfce4-goodies xrdp -y > /dev/null 2>&1
 echo "exec xfce4-session" > ~/.chrome-remote-desktop-session
 chmod +x ~/.chrome-remote-desktop-session
 sudo apt remove gnome-terminal -y > /dev/null 2>&1
