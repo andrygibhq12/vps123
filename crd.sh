3cd88e65ba319f7747bdd313195c2ea973e4f46b
andrygibhq12 <andrygibhq12@gmail.com>
2025-11-11 12:04:27 +0700

    Update crd.sh

--- crd.sh
+++ crd.sh
@@ -8,9 +8,10 @@ sudo dpkg -i chrome-remote-desktop_current_amd64.deb > /dev/null 2>&1
 sudo apt --fix-broken install -y > /dev/null 2>&1
 
 echo "===> Installing Xfce"
-sudo apt install xfce4 xfce4-goodies -y > /dev/null 2>&1
+sudo apt install xfce4 desktop-base xfce4-terminal -y > /dev/null 2>&1
 echo "exec xfce4-session" > ~/.chrome-remote-desktop-session
 chmod +x ~/.chrome-remote-desktop-session
+sudo apt remove gnome-terminal -y > /dev/null 2>&1
 
 echo "===> Installing Gdown"
 sudo apt install python3-pip -y > /dev/null 2>&1
